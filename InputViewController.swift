3a4176c1c2d82e988e1807eccc23083dcea08cd6
tyoko <tamoriinu@gmail.com>
2016-09-11 21:40:52 +0900

    カテゴリ項目追加

--- InputViewController.swift
+++ InputViewController.swift
@@ -12,6 +12,7 @@ import RealmSwift
 class InputViewController: UIViewController {
     
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UITextField!
     @IBOutlet weak var contentsTextView: UITextView!
     @IBOutlet weak var datePicker: UIDatePicker!
     
@@ -26,6 +27,7 @@ class InputViewController: UIViewController {
         self.view.addGestureRecognizer(tapGesture)
         
         titleTextField.text = task.title
+        categoryTextField.text = task.category
         contentsTextView.text = task.contents
         datePicker.date = task.date
         
@@ -40,6 +42,7 @@ class InputViewController: UIViewController {
     override func viewWillDisappear(animated: Bool) {
         try! realm.write {
             self.task.title = self.titleTextField.text!
+            self.task.category = self.categoryTextField.text!
             self.task.contents = self.contentsTextView.text!
             self.task.date = self.datePicker.date
             self.realm.add(self.task, update: true)
